13415059b1436692e8f26cadbadaff5500259960
125365236+TBWanderer@users.noreply.github.com <218463407+keepinfov@users.noreply.github.com>
2025-07-20 21:29:05 +0300

    Remade gopls setup

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -2,4 +2,4 @@ local lspconfig = require('lspconfig')
 lspconfig.clangd.setup {}
 lspconfig.rust_analyzer.setup {}
 lspconfig.pyright.setup {}
-lsp.gopls.setup {}
+lspconfig.gopls.setup { settings = { gopls = { hints = { compositeLiteralFields = true, parameterNames = true } } } }
