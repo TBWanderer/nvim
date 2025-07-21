2ed866241797e7d2536166fac97ccfa45c7120a7
125365236+TBWanderer@users.noreply.github.com <218463407+keepinfov@users.noreply.github.com>
2025-07-21 17:56:26 +0300

    Removed clangd

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,5 +1,4 @@
 local lspconfig = require('lspconfig')
-lspconfig.clangd.setup {}
 lspconfig.rust_analyzer.setup {}
 lspconfig.pyright.setup {}
 lspconfig.gopls.setup { settings = { gopls = { hints = { compositeLiteralFields = true, parameterNames = true } } } }
